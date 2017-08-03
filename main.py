d6288099f5fc31e1b2c37ca413860881e5ff3c1e
harsh <harsh7oct@gmail.com>
2017-08-03 11:21:03 +0530

    concatination of salutation and name

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print 'Hello World'
+print "Let's get started"
+spy_salutation = raw_input("What should we call you? Mr. or Ms. : ")
+spy_name = raw_input("Provide your name here :")
+# concatination of salutation and name.
+spy_name = spy_salutation + "" + spy_name
+print "welcome "+ spy_name + " Glad to have you back with us."
