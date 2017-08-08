f4d6faf71a3f69279853f4864370f4ef21e18049
harsh <harsh7oct@gmail.com>
2017-08-08 10:45:07 +0530

    name can't be empty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 print "Let's get started"
+spy_name = raw_input("what is your name ?")
+if len(spy_name)>0:
+    print "VALID NAME"
 spy_salutation = raw_input("What should we call you? Mr. or Ms. : ")
-spy_name = raw_input("Provide your name here :")
 # concatination of salutation and name.
 spy_name = spy_salutation + "" + spy_name
 print "welcome "+ spy_name + " Glad to have you back with us."
