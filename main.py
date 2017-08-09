856b9a20ee477d7cd318e08b476d6a0cf5433b53
harsh <harsh7oct@gmail.com>
2017-08-09 14:41:20 +0530

    import a file

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#import statement
+from spy_details import spy_name,spy_salutation,spy_rating,spy_age
+
+
 print "Let's get started"
 spy_name = raw_input("what is your name ?")
 if len(spy_name) > 0:
@@ -6,6 +10,12 @@ if len(spy_name) > 0:
     spy_name = spy_salutation + "" + spy_name
     print "welcome " + spy_name + " Glad to have you back with us."
     print "ALRIGHT" + spy_name +" I WOULD LIKE TO KNOW MORE ABOUT YOU"
+    spy_age = 0
+    spy_age = int(raw_input("enter the age of the spy"))
+    print type(spy_age)
+    spy_rating = 0.0
+    spy_rating = float(raw_input("enter the rating of the spy"))
+    print type(spy_rating)
 else :
     print "not valid"
 
---
+++ spy_details.py
@@ -0,0 +1,5 @@
+spy_name = "harsh"
+spy_age = 21
+spy_salutation = "mr."
+spy_rating = 4.7
+
